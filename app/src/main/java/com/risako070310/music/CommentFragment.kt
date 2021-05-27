ce09c5a487ddf314dafbbe8a2dd8bc3d6cd1f6f0
risako070310 <risako070310@gmail.com>
2021-05-27 23:10:48 +0900

    update firebase data content

--- app/src/main/java/com/risako070310/music/CommentFragment.kt
+++ app/src/main/java/com/risako070310/music/CommentFragment.kt
@@ -2,20 +2,24 @@ package com.risako070310.music
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_comment.*
+import java.time.LocalDateTime
 
 class CommentFragment : Fragment() {
 
     private val db = Firebase.firestore
+    private val songId = arguments?.getString("songId")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,16 +28,24 @@ class CommentFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_comment, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val song = "はしりがき"
+        val artist = "マカロニえんぴつ"
+        val imageURL = "https://i.scdn.co/image/ab67616d00001e02651f95448e23830d42f141eb"
+        val spotifyURL = "https://open.spotify.com/track/4rVpueOWFM9jUjZSWt1eNu?si=g19PvA_8SpSTxJYkXif_-Q"
 
         nextButton.setOnClickListener {
             val user = hashMapOf(
                 "user-name" to arguments?.getString("name"),
-                "song" to "はしりがき",
-                "artist" to "マカロニえんぴつ",
+                "song" to song,
+                "artist" to artist,
                 "message" to commentEditText.text.toString(),
-                "imageURL" to "https://i.scdn.co/image/ab67616d00001e02651f95448e23830d42f141eb"
+                "imageURL" to imageURL,
+                "spotifyURL" to spotifyURL,
+                "updateTime" to LocalDateTime.now()
             )
 
             db.collection("users")
